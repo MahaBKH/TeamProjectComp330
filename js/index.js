97581642dc66998292a9e6f9b27836f6aaf2ecd9
doyewo <dexteroyewo@gmail.com>
2019-02-28 20:36:36 -0600

    yaymerge

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function getResults() {
+    var user = document.getElementById("user").getValues;
+    var hash = document.getElementById("hashtag").getValues;
+    var www = document.getElementById("URL").getValues; 
+   
+    console.log( $('#user').get(0));  
+}
--- twitter.js
+++ twitter.js
@@ -5,12 +5,15 @@ var Twit = require ('twit');
     access_token: '1097522174326464512-P0Tzdwpong9efNuKZT9eFjm42r9x7F',
     access_token_secret: 'cTSYXbqHtrFYLclGlRrzBFDwq3zh1MZNU1H5itdKU9FOa'
 });
-T.get('search/tweets', { q: 'Loyola since:2018-11-02', count: 100 }, function(err, data, response) {
+
+function searchAPI(hashtag, user, url){
+T.get('search/tweets', { q: 'Loyola since:2018-11-02', count: 5 }, function(err, data, response) {
     data.statuses.forEach(function(tweet){
         console.log("tweet:" + tweet.text)
     })
-    // console.log(data)
+     console.log(data['text'])
   })
-  
+}
 
-var loginBTN = document.getElementById('loginBTN').addEventListener('click', yourfunction);
+searchAPI('d','d','d')
+//var loginBTN = document.getElementById('loginBTN').addEventListener('click', yourfunction);
